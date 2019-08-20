cedfdd36bbedaa53a64722c865bf500e363a9a2f
farahbousselmi <53794189+farahbousselmi@users.noreply.github.com>
2019-08-20 15:19:02 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>00:30:00</p>
+      <div className="Timer-property">
+        <h5>Hour</h5>
+        <h5>Minutes</h5>
+        <h5>Second</h5>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
